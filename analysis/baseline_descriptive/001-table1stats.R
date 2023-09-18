0dcf9496c4cdfa6a9073e50aaf1b4b61cc5a5a81
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-09-18 17:37:33 +0100

    create baseline stats analysis code

---
+++ analysis/baseline_descriptive/001-table1stats.R
@@ -0,0 +1,157 @@
+library(tidyverse)
+library(gt)
+library(gtsummary)
+library(here)
+
+## create folder for outputs to go to
+output_dir_tab <- here("output/tables")
+fs::dir_create(output_dir_tab)
+
+## source useful files
+source(here("analysis/R_fn/redaction.R"))
+
+## set redaction threshold
+threshold <- 5
+
+# read_in_data ------------------------------------------------------------
+op_neat <- read_csv(here("output/openprompt_raw.csv.gz"))
+
+op_tpp <- read_csv(here("output/openprompt_linked_tpp_edited.csv.gz"))
+op_master <- op_neat %>% 
+  left_join(op_tpp, by = "patient_id")
+
+op_table_stats <- op_master %>% 
+  filter(survey_response == 1) %>% 
+  mutate(
+    tpp_data = !is.na(age)
+    ) %>% 
+  dplyr::select(
+    base_gender,
+    base_ethnicity,
+    base_highest_edu,
+    base_disability,
+    base_relationship,
+    base_hh_income,
+    tpp_data,
+    age,
+    age_cat,
+    practice_nuts,
+    imd_q5,
+    rural_urban,
+    hh_size,
+    comorbidities,
+    n_lc_records,
+    all_test_positive,
+    no_prev_vacc
+    )
+
+var_labels <- list(
+  base_gender ~ "Gender (app question)",
+  base_ethnicity ~ "Ethnicity (app question)",
+  base_highest_edu ~ "Highest education (app question)",
+  base_disability ~ "Disability (app question)",
+  base_relationship ~ "Relationship status (app question)",
+  base_hh_income ~ "Household income (app question)",
+  tpp_data ~ "Linked records available",
+  age ~ "Age",
+  age_cat ~ "Age category",
+  practice_nuts ~ "Region",
+  imd_q5 ~ "IMD (quintile)",
+  rural_urban ~ "Rural/urban classification",
+  hh_size ~ "Household compostion (2020)",
+  comorbidities ~ "Comorbidities",
+  n_lc_records ~ "Number of Long COVID records",
+  all_test_positive  ~ "Number of positive COVID-19 test",
+  no_prev_vacc ~ "Number of previous COVID-19 vaccinations"
+)
+
+var_labels <- var_labels %>%
+  set_names(., map_chr(., all.vars))
+
+tab1 <- op_table_stats %>%
+  tbl_summary(
+    statistic = list(
+      all_continuous() ~ "{mean} ({p25}-{p75})",
+      all_categorical() ~ "{n} ({p}%)"
+    ),
+    type = list(
+      base_gender ~ "categorical",
+      base_ethnicity ~ "categorical",
+      base_highest_edu ~ "categorical",
+      base_disability ~ "categorical",
+      base_relationship ~ "categorical",
+      base_hh_income ~ "categorical",
+      tpp_data ~ "categorical",
+      age ~ "continuous",
+      age_cat ~ "categorical",
+      practice_nuts ~ "categorical",
+      imd_q5 ~ "categorical",
+      rural_urban  ~ "categorical",
+      hh_size ~ "categorical",
+      comorbidities ~ "categorical",
+      n_lc_records ~ "categorical",
+      all_test_positive ~ "categorical",
+      no_prev_vacc ~ "categorical"
+    ),
+    digits = all_continuous() ~ 1
+  )
+
+## export as an html for our own review
+tab1 %>%
+  as_gt() %>%
+  gt::gtsave(
+    filename = paste0("table1.html"),
+    path = fs::path(output_dir_tab)
+  )
+
+# redact and export raw statistics ----------------------------------------
+raw_stats <- tab1$meta_data %>%
+  select(var_label, df_stats) %>%
+  unnest(df_stats)
+
+raw_stats_redacted_catgorical <- raw_stats %>%
+  filter(!is.na(n)) %>% 
+  mutate(
+    n=redact_and_round(n, threshold),
+    N=redact_and_round(N, threshold),
+    p=round(100*n/N,1),
+    N_miss = redact_and_round(N_miss, threshold),
+    N_obs = redact_and_round(N_obs, threshold),
+    p_miss = round(100*N_miss/N_obs,1),
+    N_nonmiss = redact_and_round(N_nonmiss, threshold),
+    p_nonmiss = round(100*N_nonmiss/N_obs,1),
+    var_label = factor(var_label, levels = names(var_labels), labels = map_chr(var_labels, ~last(as.character(.)))),
+    variable_levels = replace_na(as.character(variable_levels), "")
+  ) %>% 
+  mutate(prettyN = as.character(prettyNum(formatC(n, digits = 1, format = "f"), 
+                                          big.mark = ",", preserve.width = "none",
+                                          drop0trailing = TRUE)),
+         stat = paste0(prettyN, " (",p,"%)")) %>% 
+  dplyr::select(variable = var_label, 
+                level = variable_levels, 
+                n, p,
+                stat)
+
+raw_stats_redacted_numeric <- raw_stats %>% 
+  filter(is.na(n)) %>% 
+  mutate_at(c("mean", "p25", "p75"), ~prettyNum(formatC(., digits = 1, format = "f"), 
+                                   big.mark = ",", preserve.width = "none",
+                                   drop0trailing = TRUE)) %>% 
+  mutate(
+    stat = paste0(mean, " (", p25, "-", p75, ")"),
+    var_label = factor(
+      var_label,
+      levels = names(var_labels),
+      labels = map_chr(var_labels, ~ last(as.character(.)))
+    )
+  ) %>% 
+  dplyr::select(variable = var_label, 
+                  level = variable_levels,
+                  n = N_nonmiss,
+                  p = p_nonmiss,
+                  stat) 
+  
+raw_stats_output <- raw_stats_redacted_catgorical %>% 
+  bind_rows(raw_stats_redacted_numeric)
+
+write_csv(raw_stats_output, paste0(here::here("output/tables/table1_stats.csv")))
---
+++ analysis/baseline_descriptive/002-map-making.R
@@ -0,0 +1,148 @@
+library(tidyverse)
+library(sf)
+library(janitor)
+library(here)
+library(cowplot)
+
+## create folder for outputs to go to
+output_dir_tab <- here("output/plots")
+fs::dir_create(output_dir_tab)
+
+## source useful files
+source(here("analysis/R_fn/redaction.R"))
+source(here("analysis/R_fn/ggplot_theme.R"))
+
+## set redaction threshold
+threshold <- 5
+
+# read_in_data ------------------------------------------------------------
+if(!file.exists(here("data/NUTS_Level_1_(January_2018)_Boundaries.shp"))) {
+  stop( "You need to download the NUTS_Level_1_(January_2018)_Boundaries.shp files from https://geoportal.statistics.gov.uk" )
+} else if (!file.exists(here("output/tables/table1_stats.csv"))) {
+  stop( "You need to release output/table1_stats.csv from the secure server" )
+} else{
+  ## import the regional distribution stats 
+  table1_stats <- read_csv(here("output/tables/table1_stats.csv"))
+  
+  ## p1A - the map!
+  region_dist <- table1_stats %>% 
+    filter(variable == "Region") %>% 
+    rename("region_dist" = "p",
+           "region" = "level")
+  
+  nuts_shp <- st_read("data/NUTS_Level_1_(January_2018)_Boundaries.shp")
+  
+  nuts_shp_eng <- nuts_shp %>% 
+    filter(stringr::str_detect(nuts118nm, "England|Yorkshire|London")) %>% 
+    mutate(region = stringr::str_remove_all(nuts118nm, " \\(England\\)"))
+  
+  coverage_plot <- nuts_shp_eng %>%
+    left_join(region_dist, by="region") %>% 
+    ggplot(aes(geometry = geometry, fill=region_dist)) +
+    geom_sf(lwd = .25, colour='black') +
+    geom_sf_label(aes(label = paste0(round(region_dist,1),"%")),
+                  label.size = 0.1, 
+                  colour = "white",
+                  label.r = unit(0.5, "lines"),
+                  fun.geometry = st_centroid,
+                  show.legend = F) +
+    theme(legend.position = "none",
+          legend.text.align = 1,
+          panel.background=element_rect(fill="white")) + 
+    #guides(fill=guide_legend(title="OpenPROMPT cohort (%)")) + 
+    xlab("") + ylab("")
+  
+  ggsave(filename=here::here("output", "plots","openprompt_dist_map.tiff"),coverage_plot,dpi=600,width = 20,height = 20, units = "cm")
+  
+  ## p1B - age distribution
+  categorical_data <- table1_stats %>% 
+    filter(!is.na(level)) %>% 
+    mutate(app_or_tpp = factor(str_detect(variable, "app question"), labels = c("TPP", "App")),
+           level_temp = paste(variable, level, sep = "_"),
+           level_temp2 = factor(level_temp, levels = level_temp, labels = level)) 
+  
+  # little function to suppress the facet_grid labels so they are not repeated
+  wrapper <- function(string) {
+    stringr::str_wrap(string, width = 25)
+  }
+  
+  barcharts <- function(data_in){
+    
+    # colour pallette
+    pal <- ifelse(data_in$app_or_tpp[1]=="TPP", "A", "D")
+    
+    # position dodge
+    pd <- position_dodge2(width = 0.9)
+    
+    # explicitly order the facet grid
+    facet_order <- unique(data_in$variable)[order(unique(data_in$variable), decreasing = FALSE)]
+    
+    # get x-axis labels
+    labeller <- data_in %>% 
+      group_by(level_temp) %>% 
+      slice(1) %>% 
+      dplyr::select(level, level_temp)
+    
+    labeller2 <- labeller$level
+    names(labeller2) <- labeller$level_temp
+    
+    data_in %>% 
+      arrange(variable, level) %>% 
+      mutate_if(is_character, ~factor(.)) %>% 
+      mutate(level_temp = forcats::fct_reorder(.f = level_temp, .x = levels(level_temp), .desc = TRUE)) %>% 
+      mutate(nice_n = prettyNum(n, big.mark = ","),
+             label = paste0(level_temp2, ": ", nice_n),
+             variable = factor(variable, levels = facet_order)) %>% 
+      # make the plot
+      ggplot(aes(x = level_temp, y = p, fill = level_temp)) +
+      geom_col(alpha = 0.8, lty = 0, position = pd) +
+      scale_fill_viridis_d(option = pal) +
+      geom_label(aes(label = nice_n), fill = NA, colour = "black", position = pd, label.size = NA, hjust = 0) +
+      labs(x = "", y = "Percent (%)") +
+      coord_flip() +
+      scale_y_continuous(limits=c(0,100)) +
+      scale_x_discrete(labels = labeller2) + 
+      guides(fill = "none", label = "none") +
+      facet_grid(
+        rows = vars(variable),
+        scales = "free",
+        space = "free",
+        switch = "y", 
+        labeller = labeller(variable = wrapper)
+      ) +
+      theme_classic() +
+      theme(
+        panel.background = element_blank(), 
+        title = element_text(size = 9),
+        strip.background = element_rect(colour = NA, fill = NA),
+        strip.text.x = element_text(face = "bold", size = 9),
+        strip.text.y.left = element_text(size = 9, angle = 0, hjust = 0, vjust = 0.9, face = 3),
+        strip.placement = "outside",
+        panel.border = element_rect(fill = NA, color = NA),
+        axis.line=element_line(),
+        legend.position = "right",
+        legend.title = element_text(face = "bold", size = 10),
+        axis.text.x = element_text(face = "bold"),
+        panel.grid.major.y = element_blank(), 
+        axis.title = element_text(size = 10, face = "bold"),
+        plot.title = element_text(
+          face = "bold",
+          hjust = 0.5,
+          size = 13
+        )
+      ) 
+  }
+  tpp_data <- categorical_data %>% filter(app_or_tpp == "TPP", variable != "Region")
+  app_data <- categorical_data %>% filter(app_or_tpp == "App")
+  p1b <- barcharts(tpp_data)
+  p1c <- barcharts(app_data)
+ 
+  ## combine the plots and output
+  figure2 <- cowplot::plot_grid(
+    cowplot::plot_grid(p1c, ncol = 1, labels = c("A")), 
+    cowplot::plot_grid(coverage_plot, p1b, nrow = 1, ncol = 2, labels = c("B", "C"), rel_widths = c(0.4, 0.7)),
+    ncol = 1, nrow = 2, rel_heights = c(1.025, 1))
+  
+  ggsave(filename=here::here("output", "plots","openprompt_figure2.tiff"), figure2, dpi=450, width = 14, height = 12, units = "in", bg = "white")
+  ggsave(filename=here::here("output", "plots","openprompt_figure2.png"),  figure2, dpi=450, width = 14, height = 12, units = "in", bg = "white")
+}
