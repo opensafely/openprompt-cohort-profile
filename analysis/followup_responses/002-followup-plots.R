3f60a398504a14379c7f6e8d666fb0313afa0489
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-09-20 13:18:23 +0100

    plot follow up as count or proportion

--- analysis/followup_responses/002-followup-plots.R
+++ analysis/followup_responses/002-followup-plots.R
@@ -71,59 +71,70 @@ if (!file.exists(here("output/tables/table2_fup_stats.csv"))) {
   pals <- c("Greens", "Reds", "Oranges", rep("Blues", 5))
   names(pals) <- catvars
   
-  
-  plot_some_categorical_variables <- function(data_in, stringsearch){
-    data_plot <- data_in %>% 
-      filter(stringr::str_detect(variable, stringsearch)) %>% 
-      dplyr::select(variable, level, n, p, survey_response) %>% 
-      filter(level != "(Missing)")
-    
-    data_plot$level <- forcats::as_factor(data_plot$level)
-    levels(data_plot$level) <- stringr::str_to_title(levels(data_plot$level))
-    if(stringsearch == "breathlessness"){
-      levels(data_plot$level) <- stringr::str_sub(levels(data_plot$level), 1, 7)
+  plot_n_or_p <- function(yy){
+    plot_some_categorical_variables <- function(data_in, stringsearch, yaxis = "n"){
+      data_plot <- data_in %>% 
+        filter(stringr::str_detect(variable, stringsearch)) %>% 
+        dplyr::select(variable, level, n, p, survey_response) %>% 
+        filter(level != "(Missing)")
+      
+      data_plot$level <- forcats::as_factor(data_plot$level)
+      levels(data_plot$level) <- stringr::str_to_title(levels(data_plot$level))
+      if(stringsearch == "breathlessness"){
+        levels(data_plot$level) <- stringr::str_sub(levels(data_plot$level), 1, 7)
+      }
+      data_plot$nice_n <- prettyNum(data_plot$n, big.mark = ",")
+      
+      legend <- ifelse(stringr::str_detect(stringsearch, "COVID"), "right", "bottom")
+      barplot <- ggplot(data_plot, aes(x = survey_response, y = get(yaxis), label = nice_n, colour = level, fill = level)) +
+        geom_col(alpha = 0.4, position = pd) +
+        facet_wrap(~variable, ncol = 1) +
+        scale_fill_brewer(palette = pals[stringsearch]) +
+        scale_colour_brewer(palette = pals[stringsearch]) +
+        labs(y = "n", x = "Survey round", colour = "Response", fill = "Response") +
+        theme_ali() +
+        theme(strip.text = element_text(face = "bold", hjust = 0, colour = "gray20"),
+              legend.position = legend,
+              panel.grid.major.y = element_blank(),
+              axis.line=element_line())
+      if(yaxis == "p"){
+        barplot +
+          ylim(c(0,100)) +
+          geom_label(fill = NA, colour = "black", position = pd, label.size = NA, vjust = 0)
+      }else{
+        barplot
+      }
     }
-    data_plot$nice_n <- prettyNum(data_plot$n, big.mark = ",")
     
-    legend <- ifelse(stringr::str_detect(stringsearch, "COVID"), "right", "bottom")
-    ggplot(data_plot, aes(x = survey_response, y = p, label = nice_n, colour = level, fill = level)) +
-      geom_col(alpha = 0.4, position = pd) +
-      facet_wrap(~variable, ncol = 1) +
-      geom_label(fill = NA, colour = "black", position = pd, label.size = NA, vjust = 0) +
-      scale_fill_brewer(palette = pals[stringsearch]) +
-      scale_colour_brewer(palette = pals[stringsearch]) +
-      ylim(c(0,100)) +
-      labs(y = "%", x = "Survey round", colour = "Response", fill = "Response") +
-      theme_ali() +
-      theme(strip.text = element_text(face = "bold", hjust = 0, colour = "gray20"),
-            legend.position = legend,
-            panel.grid.major.y = element_blank(),
-            axis.line=element_line())
+    p3a <- plot_some_categorical_variables(tab2stats, catvars[1], yaxis = yy)
+    p3b <- plot_some_categorical_variables(tab2stats, catvars[2], yaxis = yy)
+    p3c <- plot_some_categorical_variables(tab2stats, catvars[3], yaxis = yy)
+    p3da <- plot_some_categorical_variables(tab2stats, catvars[4], yaxis = yy)
+    p3db <- plot_some_categorical_variables(tab2stats, catvars[5], yaxis = yy)
+    p3dc <- plot_some_categorical_variables(tab2stats, catvars[6], yaxis = yy)
+    p3dd <- plot_some_categorical_variables(tab2stats, catvars[7], yaxis = yy)
+    p3de <- plot_some_categorical_variables(tab2stats, catvars[8], yaxis = yy)
+  
+    ## 3 columns. FACIT/(EQ5D,MRC)/COVIDs 
+    p3 <- cowplot::plot_grid(
+      ## column 1 - FACIT only
+      cowplot::plot_grid(p3b, labels = "A"),
+      ## column 2 - Eq5d, MRC
+      cowplot::plot_grid(p3a, p3c, ncol = 1, labels = c("B", "C"), rel_heights = c(0.8, 0.2)),
+      ## column 3 - COVIDs
+      cowplot::plot_grid(p3da, p3db, p3dc, p3dd, p3de,
+                         ncol = 1, labels = LETTERS[4:8]),
+      ## combine together in a blaze of glory
+      ncol = 3
+    )  
+    p3
   }
+  p3_n <- plot_n_or_p("n")
+  ggsave(filename=here::here("output", "plots","openprompt_figure3.tiff"), p3_n, dpi=450, width = 21, height = 10, units = "in", bg = "white")
+  ggsave(filename=here::here("output", "plots","openprompt_figure3.png"),  p3_n, dpi=450, width = 21, height = 10, units = "in", bg = "white")
   
-  p3a <- plot_some_categorical_variables(tab2stats, catvars[1])
-  p3b <- plot_some_categorical_variables(tab2stats, catvars[2])
-  p3c <- plot_some_categorical_variables(tab2stats, catvars[3])
-  p3da <- plot_some_categorical_variables(tab2stats, catvars[4])
-  p3db <- plot_some_categorical_variables(tab2stats, catvars[5])
-  p3dc <- plot_some_categorical_variables(tab2stats, catvars[6])
-  p3dd <- plot_some_categorical_variables(tab2stats, catvars[7])
-  p3de <- plot_some_categorical_variables(tab2stats, catvars[8])
-
-  ## 3 columns. FACIT/(EQ5D,MRC)/COVIDs 
-  p3 <- cowplot::plot_grid(
-    ## column 1 - FACIT only
-    cowplot::plot_grid(p3b, labels = "A"),
-    ## column 2 - Eq5d, MRC
-    cowplot::plot_grid(p3a, p3c, ncol = 1, labels = c("B", "C"), rel_heights = c(0.8, 0.2)),
-    ## column 3 - COVIDs
-    cowplot::plot_grid(p3da, p3db, p3dc, p3dd, p3de,
-                       ncol = 1, labels = LETTERS[4:8]),
-    ## combine together in a blaze of glory
-    ncol = 3
-  )  
-  
-  ggsave(filename=here::here("output", "plots","openprompt_figure3.tiff"), p3, dpi=450, width = 26, height = 10, units = "in", bg = "white")
-  ggsave(filename=here::here("output", "plots","openprompt_figure3.png"),  p3, dpi=450, width = 26, height = 10, units = "in", bg = "white")
+  p3_p <- plot_n_or_p("p")
+  ggsave(filename=here::here("output", "plots","openprompt_figure3_proportion.tiff"), p3_p, dpi=450, width = 26, height = 10, units = "in", bg = "white")
+  ggsave(filename=here::here("output", "plots","openprompt_figure3_proportion.png"),  p3_p, dpi=450, width = 26, height = 10, units = "in", bg = "white")
 }
   
