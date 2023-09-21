2bcabf8b1bdd38e55e6375928b6b7a780e5d2223
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-09-21 15:31:36 +0100

    create R code to evaluate representativeness

---
+++ analysis/05_representativeness/501_demographic_representativeness.R
@@ -0,0 +1,214 @@
+library(tidyverse)
+library(gt)
+library(gtsummary)
+library(arrow)
+library(here)
+
+## create folder for outputs to go to
+output_dir_tab <- here("output/tables")
+fs::dir_create(output_dir_tab)
+
+## source useful files
+source(here("analysis/R_fn/redaction.R"))
+source(here("analysis/R_fn/ggplot_theme.R"))
+
+## set redaction threshold
+threshold <- 7
+
+# read_in_data ------------------------------------------------------------
+op_neat <- arrow::read_parquet(here("output/openprompt_raw.gz.parquet"))
+os_demo <- read_csv(here("output/opensafely_tpp_demographics.csv.gz"))
+
+op_master <- op_neat %>% 
+  filter(survey_response == 1) %>% 
+  dplyr::select(patient_id, base_ethnicity, base_gender, base_hh_income) %>% 
+  # merge on the ages of those in openPROMPT with `left_join`
+  left_join(dplyr::select(os_demo, patient_id, age, imd), by = "patient_id") %>% 
+  rename(op_age = age,
+         op_imd = imd) %>% 
+  full_join(os_demo, by = "patient_id") %>% 
+  mutate(
+    sex = factor(sex, 
+                 levels = c("female", "intersex", "male", "unknown"),
+                 labels = c("female", "intersex/non-binary/other/refused", "male", "unknown")),
+    ethnicity = factor(
+      ethnicity,
+      levels = 1:6, 
+      labels = c(
+        "White",
+        "Mixed", 
+        "Asian/Asian Brit", 
+        "Black/African/Caribbn/Black Brit",
+        "Other/not stated",
+        "Other/not stated"
+      )),
+    imd_q5 = cut(imd,
+                 breaks = c(32844 * seq(0, 1, 0.2)),
+                 labels = c("1 (most deprived)",
+                            "2",
+                            "3",
+                            "4",
+                            "5 (least deprived)")
+    ),
+    op_imd_q5 = cut(op_imd,
+                 breaks = c(32844 * seq(0, 1, 0.2)),
+                 labels = c("1 (most deprived)",
+                            "2",
+                            "3",
+                            "4",
+                            "5 (least deprived)")
+    ),
+    # create an age category variable for easy stratification
+    age_cat = cut(
+      age, 
+      breaks = c(0, seq(25, 70, 5), Inf),
+      labels = c(
+        "18-24",
+        "25-29",
+        "30-34",
+        "35-39",
+        "40-44",
+        "45-49",
+        "50-54",
+        "55-59",
+        "60-64",
+        "65-69",
+        "70+"
+      )),
+    op_age_cat = cut(
+      op_age, 
+      breaks = c(0, seq(25, 70, 5), Inf),
+      labels = c(
+        "18-24",
+        "25-29",
+        "30-34",
+        "35-39",
+        "40-44",
+        "45-49",
+        "50-54",
+        "55-59",
+        "60-64",
+        "65-69",
+        "70+"
+      )),
+  ) %>% 
+  dplyr::select(
+    patient_id,
+    OpenPROMPT_ethnicity = base_ethnicity,
+    OpenPROMPT_gender = base_gender,
+    OpenPROMPT_hhincome = base_hh_income,
+    OpenPROMPT_age = op_age, 
+    OpenPROMPT_agecat = op_age_cat,
+    OpenPROMPT_imd = imd_q5, 
+    OpenSAFELY_ethnicity = ethnicity,
+    OpenSAFELY_gender = sex,
+    OpenSAFELY_age = age, 
+    OpenSAFELY_agecat = age_cat, 
+    OpenSAFELY_imd = imd_q5
+  )
+
+op_long <- op_master %>% 
+  pivot_longer(-patient_id, 
+               names_to = c("Var", ".value"), 
+               names_sep="_" )
+
+var_labels <- list(
+  ethnicity ~ "Ethnicity",
+  gender ~ "Gender (OpenPROMPT)/Sex (OpenSAFELY)",
+  hhincome ~ "Household income (OpenPROMPT only)",
+  imd ~ "Index of Multiple Deprivation",
+  age ~ "Age",
+  agecat ~ "Age category"
+)
+
+var_labels <- var_labels %>%
+  set_names(., map_chr(., all.vars))
+
+tab1 <- op_long %>%
+  dplyr::select(-patient_id) %>% 
+  mutate_at(c("ethnicity", "gender", "imd"), ~stringr::str_to_lower(.)) %>% 
+  #mutate_if(is.character, ~forcats::fct_explicit_na(forcats::as_factor(.))) %>% 
+  tbl_summary(
+    by = Var,
+    statistic = list(
+      all_continuous() ~ "{mean} ({p25}-{p75})",
+      all_categorical() ~ "{n} ({p}%)"
+    ),
+    type = list(
+      ethnicity ~ "categorical",
+      gender ~ "categorical",
+      hhincome ~ "categorical",
+      imd ~ "categorical",
+      age ~ "continuous",
+      agecat ~ "categorical"
+    ),
+    digits = all_continuous() ~ 1, 
+  )
+
+tab1 %>%
+  as_gt() %>%
+  gt::gtsave(
+    filename = "table3_represetativeness.html",
+    path = fs::path(output_dir_tab)
+  )
+
+# redact and export raw statistics ----------------------------------------
+raw_stats <- tab1$meta_data %>%
+  select(var_label, df_stats) %>%
+  unnest(df_stats)
+
+raw_stats_redacted_catgorical <- raw_stats %>%
+  filter(!is.na(n)) %>% 
+  mutate(
+    n=redact_and_round(n, threshold),
+    trueN=N,
+    N=redact_and_round(N, threshold),
+    p=round(100*n/N,1),
+    N_miss = redact_and_round(N_miss, threshold),
+    N_obs = redact_and_round(N_obs, threshold),
+    p_miss = round(100*N_miss/N_obs,1),
+    N_nonmiss = redact_and_round(N_nonmiss, threshold),
+    p_nonmiss = round(100*N_nonmiss/N_obs,1),
+    var_label = factor(var_label, levels = names(var_labels), labels = map_chr(var_labels, ~last(as.character(.)))),
+    variable_levels = replace_na(as.character(variable_levels), "")
+  ) %>% 
+  mutate(prettyN = as.character(prettyNum(formatC(n, digits = 1, format = "f"), 
+                                          big.mark = ",", preserve.width = "none",
+                                          drop0trailing = TRUE)),
+         stat = paste0(prettyN, " (",p,"%)")) %>% 
+  dplyr::select(variable = var_label, 
+                level = variable_levels, 
+                n, p,
+                N = trueN,
+                stat,
+                data_source = by)# %>% 
+#pivot_wider(names_from = by, values_from = stat, names_prefix = "survey_")
+
+
+raw_stats_redacted_numeric <- raw_stats %>% 
+  filter(is.na(n)) %>% 
+  mutate_at(c("mean", "p25", "p75"), ~prettyNum(formatC(., digits = 1, format = "f"), 
+                                                big.mark = ",", preserve.width = "none",
+                                                drop0trailing = TRUE)) %>% 
+  mutate(
+    stat = paste0(mean, " (", p25, "-", p75, ")"),
+    var_label = factor(
+      var_label,
+      levels = names(var_labels),
+      labels = map_chr(var_labels, ~ last(as.character(.)))
+    )
+  ) %>% 
+  dplyr::select(variable = var_label, 
+                level = variable_levels,
+                mean, p25, p75,
+                n = N_nonmiss,
+                N = N_nonmiss,
+                p = p_nonmiss,
+                stat,
+                data_source = by) #%>% 
+#pivot_wider(names_from = by, values_from = stat, names_prefix = "survey_")
+
+raw_stats_output <- raw_stats_redacted_catgorical %>% 
+  bind_rows(raw_stats_redacted_numeric)
+
+write_csv(raw_stats_output, here::here("output/tables/table3_rep_stats.csv"))
---
+++ analysis/05_representativeness/502_representativeness_plot.R
@@ -0,0 +1,149 @@
+library(tidyverse)
+library(janitor)
+library(RColorBrewer)
+library(forcats)
+library(here)
+library(cowplot)
+
+## create folder for outputs to go to
+output_dir_tab <- c(here("output/plots"), here("output/tables/"))
+for(ff in output_dir_tab){
+  fs::dir_create(output_dir_tab)
+}
+
+## source useful files
+source(here("analysis/R_fn/redaction.R"))
+source(here("analysis/R_fn/ggplot_theme.R"))
+
+## set redaction threshold
+threshold <- 7
+
+
+# check if data exists before running into errors -------------------------
+if (!file.exists(here("output/tables/table2_fup_stats.csv"))) {
+  stop( "You need to release output/table2_fup_stats.csv from the secure server" )
+} else{
+  
+  ## import the represertnativeness data
+  tab3stats <- read_csv(here("output/tables/table3_rep_stats.csv"))
+  
+  ## export the table for easy copy and paste life
+  table3 <- tab3stats %>% 
+    dplyr::select(variable, level, stat, data_source) %>% 
+    pivot_wider(names_from = data_source, values_from = stat, names_prefix = "survey_") %>% 
+    group_by(variable) %>% 
+    mutate(row_n = 1:n()) %>% 
+    ungroup() %>% 
+    mutate(variable_lab = ifelse(row_n > 1, "", variable)) %>% 
+    dplyr::select(-row_n, -variable) %>% 
+    dplyr::select(variable_lab, everything())
+  
+  
+  table3_n <- tab3stats %>% 
+    ## different numbers of people will have answered the various questions
+    group_by(data_source) %>% 
+    summarise(stat = prettyNum(max(N), big.mark = ",")) %>%
+    ungroup() %>% 
+    mutate(variable = "N") %>% 
+    ## format for bind_rows
+    pivot_wider(id_cols = variable, names_from = data_source, values_from = stat, names_prefix = "survey_") %>% 
+    mutate(level = "") %>% 
+    dplyr::select(variable_lab = variable, level, everything())
+  
+  table3 <- table3_n %>% 
+    bind_rows(table3)
+  
+  write_csv(table3, here("output/tables/table3_formatted.csv"))
+  
+  ## EQ5D bar charts 
+  
+  # age profile comparison --------------------------------------------------
+  age_cat_plot <- tab3stats %>% 
+    filter(variable == "Age category") %>% 
+    dplyr::select(variable, level, data_source, p)
+  
+  p_age <- ggplot(age_cat_plot, aes(x = level, y = p, group = data_source, colour = data_source, linetype = data_source)) + 
+    geom_point(pch = 1, size = 5) + 
+    geom_line(linewidth = 1.1) +
+    scale_colour_manual(values = c("indianred1", "darkslategray4")) + 
+    labs(x = "Age category", y = "%", colour = "Data source", linetype = "Data source") +
+    theme_ali() +
+    theme(panel.grid.major.y = element_blank(),
+          legend.position = "top") 
+  
+  ## and the rest
+  wrapper <- function(string) {
+    stringr::str_wrap(string, width = 25)
+  }
+  
+  tab3plot <- tab3stats %>% 
+    filter(is.na(mean)) %>% 
+    mutate(level_temp = paste(variable, level, sep = "_"),
+           level_temp2 = factor(level_temp, levels = level_temp, labels = level))
+  
+  # position dodge
+  pd <- position_dodge2(width = 0.9)
+  
+  # explicitly order the facet grid
+  facet_order <- unique(data_in$variable)[order(unique(data_in$variable), decreasing = FALSE)]
+  if(sum(facet_order=="Linked records available")==1){
+    facet_order <- c("Linked records available", facet_order[facet_order!="Linked records available"])
+  }
+  
+  # get x-axis labels
+  labeller <- tab3plot %>% 
+    group_by(level_temp) %>% 
+    slice(1) %>% 
+    dplyr::select(level, level_temp)
+  
+  labeller2 <- labeller$level
+  names(labeller2) <- labeller$level_temp
+  
+  barplotting <- function(var, plot_legend = FALSE){
+    lp <- ifelse(plot_legend, "top", "none")
+    tab3plot %>% 
+      filter(variable == var) %>% 
+    ggplot(aes(x = level_temp, y = p, fill = data_source)) + 
+      geom_col(position = pd) +
+      scale_x_discrete(labels = labeller2) + 
+      scale_fill_manual(values = c("indianred1", "darkslategray4")) +
+      labs(x = {var}, y = "Percent (%)", fill = "Data source") +
+      theme_classic() +
+      theme(
+        panel.background = element_blank(), 
+        title = element_text(size = 9),
+        strip.background = element_rect(colour = NA, fill = NA),
+        strip.text.x = element_text(face = "bold", size = 9),
+        strip.text.y.left = element_text(size = 9, angle = 0, hjust = 0, vjust = 0.9, face = 3),
+        strip.placement = "outside",
+        panel.border = element_rect(fill = NA, color = NA),
+        axis.line=element_line(),
+        legend.position = lp,
+        legend.title = element_text(face = "bold", size = 10),
+        axis.text.x = element_text(face = "bold", angle = 10, hjust = 0.95),
+        panel.grid.major.y = element_blank(), 
+        axis.title = element_text(size = 10, face = "bold"),
+        plot.title = element_text(
+          face = "bold",
+          hjust = 0.5,
+          size = 13
+        )
+      ) 
+  }
+  #p1 <- barplotting("Age category", plot_legend = TRUE)
+  p2 <- barplotting("Ethnicity")
+  p3 <- barplotting("Gender (OpenPROMPT)/Sex (OpenSAFELY)")
+  p4 <- barplotting("Household income (OpenPROMPT only)")
+  p5 <- barplotting("Index of Multiple Deprivation")
+  
+  figure4 <- cowplot::plot_grid(
+    plot_grid(p_age, labels = LETTERS[1], ncol = 1),
+    plot_grid(p2, p3, p4, p5, ncol = 2, labels = LETTERS[2:5]),
+    ncol = 1, rel_heights = c(0.4, 0.7)
+  )
+  
+  ggsave(filename=here::here("output", "plots","openprompt_figure4.tiff"), figure4, dpi=450, width = 13, height = 8, units = "in", bg = "white")
+  ggsave(filename=here::here("output", "plots","openprompt_figure4.png"),  figure4, dpi=450, width = 13, height = 8, units = "in", bg = "white")
+
+}
+
